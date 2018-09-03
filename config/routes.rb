5133cf86813368703ca6ab4d8f1b323e9115f799
lucas <lucasprograms@gmail.com>
2018-09-02 20:30:55 -0400

    remove extra route and update session controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+  #static
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   
+  #users
   resources :users
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
 
+  #log in
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
